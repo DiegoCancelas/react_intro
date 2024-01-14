b527f291260c37ddf009138beadc300962c0ffdc
diego_cancelas <diegocancelas@gmail.com>
2024-01-14 16:05:10 -0300

    criando e estilizando componentes

--- src/componentes/Componentes.js
+++ src/componentes/Componentes.js
@@ -1,12 +1,13 @@
 import React from 'react' // importação da biblioteca
+import './Componente.css'
 
 const Componente = () => (  //definição do componente
       
-    <>
+    <div className='Componente'>
         <h3>componente</h3> 
         <p>adicionando um parágrafo e um botao logo abaixo</p>  
         <button>botao</button>
-    </>
+    </div>
 );
 
 export default Componente;  // exportação do componente
