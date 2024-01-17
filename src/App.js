56013ca02037775ea0966cc5f8d35312b634b11a
diego_cancelas <diegocancelas@gmail.com>
2024-01-16 22:14:50 -0300

    props - intro

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {     // inserção do componente na função 'App'
     <div className="App">
       <header className="App-header">
         <p>testando componentes</p>
-        <Componente />   
-        <Componente />   
+        <Componente nome='maria eduarda' email='mEduarda@gmail.com' data = { new Date (2024,4,12) } >   
+        </Componente >   
+        <Componente nome ='ana luiza mendes' email='anaLu@gmail.com' data= {new Date (2024, 6, 10)}>
+        </Componente>
       </header>
     </div>
   );
--- src/componentes/Componentes.js
+++ src/componentes/Componentes.js
@@ -1,13 +1,13 @@
 import React from 'react' // importação da biblioteca
 import './Componente.css'
 
-const Componente = () => (  //definição do componente
-      
-    <div className='Componente'>
-        <h3>componente</h3> 
-        <p>adicionando um parágrafo e um botao logo abaixo</p>  
-        <button>botao</button>
-    </div>
-);
+const Componente = props => ( //definição do componente
+   
+       <div className='Componente'>
+          <h3>{props.nome}</h3> 
+          <p>{props.email}</p>
+          <p>{props.data.toString()}</p>
+       </div> 
+     );
 
 export default Componente;  // exportação do componente
